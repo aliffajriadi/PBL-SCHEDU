d2686b9446d8ebef8edef6e1de66758eb89d0f76
aliffajriadi <aliffajriadi@gmail.com>
2025-06-09 23:21:28 +0700

    fix: active ui if same path routes

--- resources/views/components/layoutComponent/SidebarDesktop.blade.php
+++ resources/views/components/layoutComponent/SidebarDesktop.blade.php
@@ -36,14 +36,21 @@
             </a>
         @endif
 
-        <a href="/notification"
-            class="flex items-center gap-2 p-2 hover:bg-green-400 rounded-xl cursor-pointer menu-item">
+        @php
+            $notifUrl = $role == "staff" ? "/staff/notification" : "/notification";
+            $isActive = request()->is(ltrim($notifUrl, '/')) ? 'bg-green-400' : '';
+        @endphp
+
+        <a href="{{ $notifUrl }}"
+            class="flex items-center gap-2 p-2 hover:bg-green-400 rounded-xl cursor-pointer menu-item {{ $isActive }}">
             <div class="flex items-center gap-2">
                 <img src="{{ asset('assets/notifications.svg') }}" class="w-5 h-auto" />
                 <p class="font-semibold">Notification</p>
             </div>
-            <p id="side-notif-badge" class="{{ session('notification_count') ? '' : 'hidden' }} text-xs text-emerald-50 bg-red-500 rounded-full px-2 py-1">{{ session('notification_count') }}</p>
-
+            <p id="side-notif-badge"
+                class="{{ session('notification_count') ? '' : 'hidden' }} text-xs text-emerald-50 bg-red-500 rounded-full px-2 py-1">
+                {{ session('notification_count') }}
+            </p>
         </a>
 
     </div>
@@ -57,39 +64,39 @@
             </a>
 
             @if(Request::segment(1) === 'group' && Request::segment(2) !== null)
-            <div class="ps-5">
-                <a href="/group" class="flex items-center gap-2 p-2 hover:bg-green-400 rounded-xl cursor-pointer menu-item">
-                    <img src="{{ asset('assets/Home.svg') }}" class="w-5 h-auto" />
-                    <p class="font-semibold">Group Dashboard</p>
-                </a>
-                <a href="{{ $path }}/note"
-                    class="flex items-center gap-2 p-2 hover:bg-green-400 rounded-xl cursor-pointer menu-item">
-                    <img src="{{ asset('assets/bx-notepad 2.svg') }}" class="w-5 h-auto" />
-                    <p class="font-semibold">Group Notes</p>
-                </a>
-                <a href="{{ $path }}/task"
-                    class="flex items-center gap-2 p-2 hover:bg-green-400 rounded-xl cursor-pointer menu-item">
-                    <img src="{{ asset('assets/bx-task (1) 2.svg') }}" class="w-5 h-auto" />
-                    <p class="font-semibold">Group Task</p>
-                </a>
-                <a href="{{ $path }}/schedule"
-                    class="flex items-center gap-2 p-2 hover:bg-green-400 rounded-xl cursor-pointer menu-item">
-                    <img src="{{ asset('assets/calender-white.svg') }}" class="w-5 h-auto" />
-                    <p class="font-semibold">Group Schedule</p>
-                </a>
-                @if ($role == 'teacher')
-                    <a href="{{ $path }}/settings"
-                        class="flex items-center gap-2 p-2 hover:bg-green-400 rounded-xl cursor-pointer menu-item">
-                        <img src="{{ asset('assets/setting.svg') }}" class="w-5 h-auto" />
-                        <p class="font-semibold">Group Settings</p>
+                <div class="ps-5">
+                    <a href="/group" class="flex items-center gap-2 p-2 hover:bg-green-400 rounded-xl cursor-pointer menu-item">
+                        <img src="{{ asset('assets/Home.svg') }}" class="w-5 h-auto" />
+                        <p class="font-semibold">Group Dashboard</p>
                     </a>
-                @endif
+                    <a href="{{ $path }}/note"
+                        class="flex items-center gap-2 p-2 hover:bg-green-400 rounded-xl cursor-pointer menu-item">
+                        <img src="{{ asset('assets/bx-notepad 2.svg') }}" class="w-5 h-auto" />
+                        <p class="font-semibold">Group Notes</p>
+                    </a>
+                    <a href="{{ $path }}/task"
+                        class="flex items-center gap-2 p-2 hover:bg-green-400 rounded-xl cursor-pointer menu-item">
+                        <img src="{{ asset('assets/bx-task (1) 2.svg') }}" class="w-5 h-auto" />
+                        <p class="font-semibold">Group Task</p>
+                    </a>
+                    <a href="{{ $path }}/schedule"
+                        class="flex items-center gap-2 p-2 hover:bg-green-400 rounded-xl cursor-pointer menu-item">
+                        <img src="{{ asset('assets/calender-white.svg') }}" class="w-5 h-auto" />
+                        <p class="font-semibold">Group Schedule</p>
+                    </a>
+                    @if ($role == 'teacher')
+                        <a href="{{ $path }}/settings"
+                            class="flex items-center gap-2 p-2 hover:bg-green-400 rounded-xl cursor-pointer menu-item">
+                            <img src="{{ asset('assets/setting.svg') }}" class="w-5 h-auto" />
+                            <p class="font-semibold">Group Settings</p>
+                        </a>
+                    @endif
 
-            </div>
+                </div>
 
             @endif
 
-            
+
         </div>
     @endif
 
@@ -122,7 +129,7 @@
             <img src="{{ asset('assets/profile.svg') }}" class="w-5 h-auto" />
             <p class="font-semibold">Edit Profile</p>
         </a>
-    
+
         <form action="{{ $role === 'staff' ? '/staff/logout' : '/logout' }}" method="POST"
             class="flex items-center gap-2 p-2 hover:bg-green-400 rounded-xl cursor-pointer menu-item">
             @csrf
@@ -132,5 +139,5 @@
             </button>
         </form>
     </div>
-    
+
 </aside>
